255f68cfdb50b3f0c1890a80e45e82b3fdbd2247
pandazyp <2823543594@qq.com>
2025-05-30 10:38:31 +0800

    [Infra] empty commit for testing ci
    issue: f-0

--- src/interpreter/quickjs/source/quickjs.cc
+++ src/interpreter/quickjs/source/quickjs.cc
@@ -24508,7 +24508,6 @@ __exception int js_parse_cond_expr(JSParseState *s, int parse_flags) {
     label1 = new_label(s);
     for (;;) {
       if (next_token(s)) return -1;
-
       emit_op(s, OP_dup);
       emit_op(s, OP_is_undefined);
       emit_goto(s, OP_if_false, label1);
