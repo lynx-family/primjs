7708cd5ccefffc86ed080666218d9e80703d17f4
pandazyp <2823543594@qq.com>
2026-02-27 16:51:56 +0800

    [BugFix] fix symbol undefined issue
    Inlcude 'inspector/interface.h' in quickjs-inner.h

--- src/interpreter/quickjs/include/quickjs-inner.h
+++ src/interpreter/quickjs/include/quickjs-inner.h
@@ -79,11 +79,7 @@ typedef int BOOL;
 
 #ifdef ENABLE_QUICKJS_DEBUGGER
 #include "inspector/debugger_inner.h"
-extern "C" {
-void HandleDebuggerException(LEPUSContext *ctx);
-void DoInspectorCheck(LEPUSContext *ctx);
-void ComputeLineCol(int64_t line_col_num, int32_t *line, int64_t *column);
-}
+#include "inspector/interface.h"
 #endif
 
 #include "gc/qjsvaluevalue-space.h"
