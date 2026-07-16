7296488c03ae9da9ad5573f604518aa7e6c0c436
Jianliang Wang <wangjianliang0@126.com>
2026-07-16 19:57:56 +0800

    [BugFix][Harmony] Fix get_exception in jsvm (#200)
    issue: None
    
    Co-authored-by: pandazyp <2823543594@qq.com>

--- src/napi/harmony/js_native_api_harmony.cc
+++ src/napi/harmony/js_native_api_harmony.cc
@@ -1,4 +1,8 @@
 
+// Copyright 2024 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
 #include "js_native_api_harmony.h"
 
 #include <ark_runtime/jsvm.h>
